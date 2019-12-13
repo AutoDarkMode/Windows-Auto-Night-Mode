b36be81ace114bf02b661a3766f136b1a70b16b6
Sam <earthstamper@gmail.com>
2019-12-13 09:23:55 +0100

    set up directory structure

---
+++ AutoThemeChangerSvc/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoThemeChangerSvc
+{
+    class Service
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
