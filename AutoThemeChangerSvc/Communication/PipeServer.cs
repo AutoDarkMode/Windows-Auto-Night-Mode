b5495011c247688fdcdcdd5ea72740b4e4db705e
Sam <earthstamper@gmail.com>
2019-12-15 21:44:38 +0100

    remove unnecessary imports

--- AutoThemeChangerSvc/Communication/PipeServer.cs
+++ AutoThemeChangerSvc/Communication/PipeServer.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.IO.Pipes;
-using System.Text;
-using System.Threading;
 
 namespace AutoThemeChangerSvc.Communication
 {
