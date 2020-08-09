404cbd66e556d08ae3e360443ae5946e38572513
Sam <earthstamper@gmail.com>
2020-08-09 21:54:00 +0200

    fix licenses

--- AutoDarkModeApp/Pages/PageAbout.xaml.cs
+++ AutoDarkModeApp/Pages/PageAbout.xaml.cs
@@ -109,7 +109,7 @@ namespace AutoDarkModeApp.Pages
         {
             string messageBoxText = "MIT License \n\n Copyright(c) 2016 minami_SC\n\n" +
                 "Permission is hereby granted, free of charge, to any person obtaining a copy" +
-                "of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights " +
+                "of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights " +
                 "to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell" +
                 "copies of the Software, and to permit persons to whom the Software is" +
                 "furnished to do so, subject to the following conditions:\n\n" +
@@ -121,8 +121,10 @@ namespace AutoDarkModeApp.Pages
                 "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" +
                 "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," +
                 "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
-            MsgBox msg = new MsgBox(messageBoxText, "FluentWPF License Information", "info", "close");
-            msg.Owner = Window.GetWindow(this);
+            MsgBox msg = new MsgBox(messageBoxText, "FluentWPF License Information", "info", "close")
+            {
+                Owner = Window.GetWindow(this)
+            };
             msg.ShowDialog();
         }
 
@@ -134,15 +136,17 @@ namespace AutoDarkModeApp.Pages
         private void TaskSchedulerLicense_MouseDown(object sender, MouseButtonEventArgs e)
         {
             string messageBoxText = "MIT Copyright (c) 2003-2010 David Hall \n\n" +
-               "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), " +
+               "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), " +
                "to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, " +
                "and/ or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n\n" +
                "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n\n" +
                "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, " +
                "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
-            MsgBox msg = new MsgBox(messageBoxText, "TaskSheduler License Information", "info", "close");
-            msg.Owner = Window.GetWindow(this);
+            MsgBox msg = new MsgBox(messageBoxText, "TaskSheduler License Information", "info", "close")
+            {
+                Owner = Window.GetWindow(this)
+            };
             msg.ShowDialog();
         }
 
@@ -169,15 +173,17 @@ namespace AutoDarkModeApp.Pages
         private void InputSimulatorLicense_MouseDown(object sender, MouseButtonEventArgs e)
         {
             string MessageBoxText = "MIT License \n\n Copyright(c) 2019 Michael Noonan \n\n" +
-                "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), " +
+                "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), " +
                 "to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or " +
                 "sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n\n" +
                 "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n\n" +
                 "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
                 "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " +
                 "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
-            MsgBox msg = new MsgBox(MessageBoxText, "InputSimulator License Information", "info", "close");
-            msg.Owner = Window.GetWindow(this);
+            MsgBox msg = new MsgBox(MessageBoxText, "InputSimulator License Information", "info", "close")
+            {
+                Owner = Window.GetWindow(this)
+            };
             msg.ShowDialog();
         }
 
@@ -197,13 +203,19 @@ namespace AutoDarkModeApp.Pages
 
         private void JsonNetLicense_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            string MessageBoxText = "Copyright (c) 2014 Newtonsoft \n\n" +
-                "This program is free software: you can redistribute it and / or modify it under the terms of the GNU Affero General Public License " +
-                "as published by the Free Software Foundation, either version 3 of the License, see http://www.gnu.org/licenses/agpl-3.0.html. \n\n" +
-                "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY " +
-                "or FITNESS FOR A PARTICULAR PURPOSE.See the GNU Affero General Public License for more details.";
-            MsgBox msg = new MsgBox(MessageBoxText, "Newtonsoft Json.NET License Information", "info", "close");
-            msg.Owner = Window.GetWindow(this);
+            string MessageBoxText = "The MIT License (MIT) \n\n" +
+                "Copyright (c) 2007 James Newton-King \n\n" +
+                "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), " +
+                "to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or " +
+                "sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n\n" +
+                "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n\n" +
+                "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
+                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " +
+                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+            MsgBox msg = new MsgBox(MessageBoxText, "Newtonsoft Json.NET License Information", "info", "close")
+            {
+                Owner = Window.GetWindow(this)
+            };
             msg.ShowDialog();
         }
 
