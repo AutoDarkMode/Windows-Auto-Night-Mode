e824b7d36d8d639603a98d1b1c9c624eb1a6e425
Sam <earthstamper@gmail.com>
2021-09-01 17:36:57 +0200

    Prepare Component Interface

---
+++ AutoDarkModeSvc/Interfaces/ISwitchComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoDarkModeSvc.Interfaces
+{
+    interface ISwitchComponent
+    {
+        public void Init();
+        public void Enabled();
+
+        /// <summary>
+        /// Switches to the desired theme
+        /// </summary>
+        /// <param name="newTheme"></param>
+        public void Switch(Theme newTheme);
+        /// <summary>
+        /// Checks if the component needs to be updated, i.e Switch() needs to be called
+        /// </summary>
+        /// <returns></returns>
+        public int PriorityToDark { get; }
+        /// <summary>
+        /// Priority for switching to light mode
+        /// </summary>
+        public int PriorityToLight { get; }
+    }
+}
