7d1f63943a38cbeb9eefcb5851099d003d1db9f7
Sam <earthstamper@gmail.com>
2021-09-06 01:36:41 +0200

    catch exception coming from service run

--- AutoDarkModeSvc/Program.cs
+++ AutoDarkModeSvc/Program.cs
@@ -24,75 +24,75 @@ namespace AutoDarkModeSvc
         [STAThread]
         static void Main(string[] args)
         {
+            //Set up Logger
+            var config = new NLog.Config.LoggingConfiguration();
+            var configDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AutoDarkMode");
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile")
+            {
+                FileName = Path.Combine(configDir, "service.log"),
+                Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss} | ${level} | " +
+                "${callsite:includeNamespace=False:" +
+                "cleanNamesOfAnonymousDelegates=true:" +
+                "cleanNamesOfAsyncContinuations=true}: ${message} ${exception:separator=|}"
+            };
+            var logconsole = new NLog.Targets.ColoredConsoleTarget("logconsole")
+            {
+                Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss} | ${level} | " +
+                "${callsite:includeNamespace=False:" +
+                "cleanNamesOfAnonymousDelegates=true:" +
+                "cleanNamesOfAsyncContinuations=true}: ${message} ${exception:separator=|}"
+            };
+
+            List<string> argsList;
+            if (args.Length > 0)
+            {
+                argsList = new List<string>(args);
+            }
+            else
+            {
+                argsList = new List<string>();
+            }
+
+            // Rules for mapping loggers to targets       
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logconsole);
+            if (argsList.Contains("/debug"))
+            {
+                config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            }
+            else
+            {
+                config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+            }
+            // Apply config           
+            LogManager.Configuration = config;
+
             try
             {
-                //Set up Logger
-                var config = new NLog.Config.LoggingConfiguration();
-                var configDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AutoDarkMode");
+                Directory.CreateDirectory(configDir);
+            }
+            catch (Exception e)
+            {
+                Logger.Fatal(e, "could not create config directory");
+            }
 
-                // Targets where to log to: File and Console
-                var logfile = new NLog.Targets.FileTarget("logfile")
+            try
+            {
+                if (!mutex.WaitOne(TimeSpan.FromSeconds(2), false))
                 {
-                    FileName = Path.Combine(configDir, "service.log"),
-                    Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss} | ${level} | " +
-                    "${callsite:includeNamespace=False:" +
-                    "cleanNamesOfAnonymousDelegates=true:" +
-                    "cleanNamesOfAsyncContinuations=true}: ${message} ${exception:separator=|}"
-                };
-                var logconsole = new NLog.Targets.ColoredConsoleTarget("logconsole")
-                {
-                    Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss} | ${level} | " +
-                    "${callsite:includeNamespace=False:" +
-                    "cleanNamesOfAnonymousDelegates=true:" +
-                    "cleanNamesOfAsyncContinuations=true}: ${message} ${exception:separator=|}"
-                };
-
-                List<string> argsList;
-                if (args.Length > 0)
-                {
-                    argsList = new List<string>(args);
-                }
-                else
-                {
-                    argsList = new List<string>();
-                }
-
-                // Rules for mapping loggers to targets       
-                config.AddRule(LogLevel.Debug, LogLevel.Fatal, logconsole);
-                if (argsList.Contains("/debug"))
-                {
-                    config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
-                }
-                else
-                {
-                    config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
-                }
-                // Apply config           
-                LogManager.Configuration = config;
-
-                try
-                {
-                    Directory.CreateDirectory(configDir);
-                }
-                catch (Exception e)
-                {
-                    Logger.Fatal(e, "could not create config directory");
-                }
-
-                try
-                {
-                    if (!mutex.WaitOne(TimeSpan.FromSeconds(2), false))
-                    {
-                        Logger.Debug("app instance already open");
-                        return;
-                    }
-                } 
-                catch (Exception ex)
-                {
-                    Logger.Error(ex, "failed getting mutex, " + ex.Message);
+                    Logger.Debug("app instance already open");
                     return;
                 }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "failed getting mutex, " + ex.Message);
+                return;
+            }
 
+            try
+            {
                 //Instantiate Runtime config
                 GlobalState.Instance();
 
@@ -111,7 +111,8 @@ namespace AutoDarkModeSvc
                     Environment.Exit(-1);
                 }
 
-                if (Builder.Config.Tunable.Debug && !argsList.Contains("/debug")) {
+                if (Builder.Config.Tunable.Debug && !argsList.Contains("/debug"))
+                {
                     config = new NLog.Config.LoggingConfiguration();
                     config.AddRule(LogLevel.Debug, LogLevel.Fatal, logconsole);
                     config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
@@ -155,6 +156,7 @@ namespace AutoDarkModeSvc
                 {
                     int.TryParse(args[0], out timerMillis);
                 }
+
                 timerMillis = (timerMillis == 0) ? TimerFrequency.Short : timerMillis;
                 Application.SetHighDpiMode(HighDpiMode.SystemAware);
                 Application.EnableVisualStyles();
@@ -162,10 +164,15 @@ namespace AutoDarkModeSvc
                 Service = new Service(timerMillis);
                 Application.Run(Service);
             }
-            finally 
+            catch (Exception ex)
+            {
+                Logger.Fatal(ex, "unhandled exception causing panic");
+            }
+            finally
             {
                 //clean shutdown
-                if (Service != null) {
+                if (Service != null)
+                {
                     Service.Cleanup();
                 }
                 mutex.Dispose();
