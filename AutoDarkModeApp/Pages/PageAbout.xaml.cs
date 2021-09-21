fd194e8e416c79116c3d93ac387c0c2ae4062974
Armin Osaj <Armin2208@hotmail.de>
2021-09-21 21:35:58 +0200

    added OpenWPF, added missing licenses, removed some links in about

--- AutoDarkModeApp/Pages/PageAbout.xaml.cs
+++ AutoDarkModeApp/Pages/PageAbout.xaml.cs
@@ -268,5 +268,57 @@ namespace AutoDarkModeApp.Pages
         {
             if (e.Key == Key.Enter) SunriseCalcLicense_MouseDown(this, null);
         }
+
+        private void ModernWPFLicense_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string MessageBoxText = "The MIT License (MIT) \n\n" +
+                "Copyright (c) 2019 Yimeng Wu \n\n" +
+                "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), " +
+                "to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or " +
+                "sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n\n" +
+                "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n\n" +
+                "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
+                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " +
+                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+            MsgBox msg = new MsgBox(MessageBoxText, "ModernWPF License Information", "info", "close");
+            msg.Owner = Window.GetWindow(this);
+            msg.ShowDialog();
+        }
+
+        private void ModernWPFLicense_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) ModernWPFLicense_MouseDown(this, null);
+        }
+
+        private void NLogLicense_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string MessageBoxText = "Copyright (c) 2004-2021 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen \n\n" +
+                "All rights reserved. \n\n" +
+                "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: \n\n" +
+                "* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. \n\n" +
+                "* Redistributions in binary form must reproduce the above copyright notice," +
+                "this list of conditions and the following disclaimer in the documentation" +
+                "and / or other materials provided with the distribution. \n\n" +
+                "* Neither the name of Jaroslaw Kowalski nor the names of its contributors may be used to endorse or promote products derived from this " +
+                "software without specific prior written permission. \n\n" +
+                "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'" +
+                "AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE" +
+                "IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE" +
+                "ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE" +
+                "LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR" +
+                "CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF" +
+                "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS" +
+                "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN" +
+                "CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE)" +
+                "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+            MsgBox msg = new MsgBox(MessageBoxText, "NLog License Information", "info", "close");
+            msg.Owner = Window.GetWindow(this);
+            msg.ShowDialog();
+        }
+
+        private void NLogLicense_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) NLogLicense_MouseDown(this, null);
+        }
     }
 }
