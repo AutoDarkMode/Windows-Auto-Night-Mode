88b5417e67011e167df3872488d4a87457fdb0bf
Sam <earthstamper@gmail.com>
2021-09-26 15:09:55 +0200

    update publish script

--- publish.bat
+++ publish.bat
@@ -1,5 +1,14 @@
+set digit1=%time:~0,1%
+if "%digit1%"==" " set digit1=0
+set timetext=%digit1%%time:~1,1%%time:~3,2%
+
+
 call dotnet publish AutoDarkModeApp\AutoDarkModeApp.csproj /p:PublishProfile=$(SolutionDir)AutoDarkModeApp\Properties\PublishProfiles\AppPublish.pubxml
 call dotnet publish AutoDarkModeSvc\AutoDarkModeSvc.csproj /p:PublishProfile=$(SolutionDir)\AutoDarkModeSvc\Properties\PublishProfiles\ServicePublish.pubxml
 call dotnet publish AutoDarkModeShell\AutoDarkModeShell.csproj /p:PublishProfile=$(SolutionDir)\AutoDarkModeShell\Properties\PublishProfiles\FolderProfile.pubxml
 call dotnet publish AutoDarkModeUpdater\AutoDarkModeUpdater.csproj /p:PublishProfile=$(SolutionDir)\AutoDarkModeUpdater\Properties\PublishProfiles\FolderProfile.pubxml
 
+tar -cvzf AutoDarkModeInstaller/Setup/ADM.zip bin/Publish
+chdir /d AutoDarkModeInstaller
+chdir /d Setup
+move ADM.zip AutoDarkModeX_Archive_%date:~-4,4%%date:~-7,2%%date:~-10,2%_%timetext%.zip
