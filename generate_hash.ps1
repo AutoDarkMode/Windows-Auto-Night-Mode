dc154f2bb216b1fa761f251ac8bb7e96dc20d2f9
Sam <earthstamper@gmail.com>
2021-10-13 14:44:18 +0200

    move deployment to own repo

--- generate_hash.ps1
+++
@@ -1,4 +0,0 @@
-#requires -PSEdition Core
-$FileHash = Get-FileHash AutoDarkModeInstaller/Setup/ADM.zip
-New-Item -Force AutoDarkModeInstaller/Setup/ADM.sha256
-Set-Content -NoNewline -Path AutoDarkModeInstaller/Setup/ADM.sha256 $FileHash.Hash
