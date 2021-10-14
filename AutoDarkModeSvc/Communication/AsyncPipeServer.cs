c58ff31fc85ae13271ee71509bb1e8408b0d8a2b
Sam <earthstamper@gmail.com>
2021-10-14 15:50:51 +0200

    remove unnecessary imports

--- AutoDarkModeSvc/Communication/AsyncPipeServer.cs
+++ AutoDarkModeSvc/Communication/AsyncPipeServer.cs
@@ -3,8 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
-using System.Linq;
-using System.ServiceModel;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
