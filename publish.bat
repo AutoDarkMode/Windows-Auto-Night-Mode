0e10fa48def5199424ebc3d6178a9130eead68a8
Sam <earthstamper@gmail.com>
2021-11-01 18:08:50 +0100

    update publish script

--- publish.bat
+++ publish.bat
@@ -2,7 +2,7 @@ REM RUST BUILD AND PUBLISH
 cargo build --release --manifest-path adm-updater-rs\Cargo.toml
 if not exist bin\Publish\Updater mkdir bin\Publish\Updater
 copy adm-updater-rs\target\release\adm-updater-rs.exe bin\Publish\Updater\AutoDarkModeUpdater.exe
-
+copy adm-updater-rs\license.html bin\Publish\Updater\license.html
 
 REM DOTNET BUILD AND PUBLISH
 call dotnet publish AutoDarkModeApp\AutoDarkModeApp.csproj /p:PublishProfile=$(SolutionDir)AutoDarkModeApp\Properties\PublishProfiles\AppPublish.pubxml
