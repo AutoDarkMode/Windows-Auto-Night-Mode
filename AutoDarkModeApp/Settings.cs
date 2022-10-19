bc37eab7e78ef491ccd735b7c8acc3ad875ce04f
Sam <earthstamper@gmail.com>
2022-10-19 14:36:25 +0200

    update all license files

--- AutoDarkModeApp/Settings.cs
+++ AutoDarkModeApp/Settings.cs
@@ -1,4 +1,20 @@
-﻿namespace AutoDarkModeApp.Properties {
+﻿#region copyright
+//  Copyright (C) 2022 Auto Dark Mode
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#endregion
+namespace AutoDarkModeApp.Properties {
     
     
     // This class allows you to handle specific events on the settings class:
--- AutoDarkModeSvc/Modules/WardenModule.cs
+++ AutoDarkModeSvc/Modules/WardenModule.cs
@@ -1,4 +1,20 @@
-﻿using AutoDarkModeLib;
+﻿#region copyright
+//  Copyright (C) 2022 Auto Dark Mode
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#endregion
+using AutoDarkModeLib;
 using AutoDarkModeLib.Configs;
 using AutoDarkModeSvc.Core;
 using AutoDarkModeSvc.Monitors;
