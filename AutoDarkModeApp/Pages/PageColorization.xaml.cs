944cdb5d54d96d8638f446423c0e07743d1f232b
Sam <earthstamper@gmail.com>
2023-05-02 20:34:34 +0200

    foss license header updates

--- AutoDarkModeApp/Pages/PageColorization.xaml.cs
+++ AutoDarkModeApp/Pages/PageColorization.xaml.cs
@@ -1,5 +1,4 @@
 ﻿#region copyright
-
 //  Copyright (C) 2022 Auto Dark Mode
 //
 //  This program is free software: you can redistribute it and/or modify
@@ -14,9 +13,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 #endregion
-
 using AutoDarkModeApp.Controls;
 using AutoDarkModeApp.Handlers;
 using AutoDarkModeLib;
