b148b21c2d8b74f78d8269e29dd5d7cf182feca0
Spiritreader <earthstamper@gmail.com>
2024-02-16 01:09:08 +0100

    rust updater publish

--- publish.bat
+++ publish.bat
@@ -1,9 +1,3 @@
-REM RUST BUILD AND PUBLISH
-cargo build --release --manifest-path adm-updater-rs\Cargo.toml
-if not exist bin\Publish\updater mkdir bin\Publish\adm-updater
-copy adm-updater-rs\target\release\adm-updater-rs.exe bin\Publish\adm-updater\AutoDarkModeUpdater.exe
-copy adm-updater-rs\license.html bin\Publish\adm-updater\license.html
-
 REM DOTNET BUILD AND PUBLISH x86
 call dotnet publish AutoDarkModeApp\AutoDarkModeApp.csproj /p:PublishProfile=$(SolutionDir)AutoDarkModeApp\Properties\PublishProfiles\AppPublish.pubxml
 call dotnet publish AutoDarkModeSvc\AutoDarkModeSvc.csproj /p:PublishProfile=$(SolutionDir)\AutoDarkModeSvc\Properties\PublishProfiles\ServicePublish.pubxml
@@ -14,3 +8,16 @@ REM DOTNET BUILD AND PUBLISH ARM64
 call dotnet publish AutoDarkModeApp\AutoDarkModeApp.csproj /p:PublishProfile=$(SolutionDir)AutoDarkModeApp\Properties\PublishProfiles\AppPublishARM64.pubxml
 call dotnet publish AutoDarkModeSvc\AutoDarkModeSvc.csproj /p:PublishProfile=$(SolutionDir)\AutoDarkModeSvc\Properties\PublishProfiles\ServicePublishARM64.pubxml
 call dotnet publish AutoDarkModeShell\AutoDarkModeShell.csproj /p:PublishProfile=$(SolutionDir)\AutoDarkModeShell\Properties\PublishProfiles\ShellPublishARM64.pubxml
+
+REM RUST BUILD AND PUBLISH
+cargo build --release --manifest-path adm-updater-rs\Cargo.toml
+cargo build --release --manifest-path adm-updater-rs\Cargo.toml --target aarch64-pc-windows-msvc
+
+if not exist bin\Publish\x86\adm-updater mkdir bin\Publish\x86\adm-updater
+if not exist bin\Publish\ARM64\adm-updater mkdir bin\Publish\ARM64\adm-updater
+
+copy adm-updater-rs\target\release\adm-updater-rs.exe bin\Publish\x86\adm-updater\AutoDarkModeUpdater.exe
+copy adm-updater-rs\license.html bin\Publish\x86\adm-updater\license.html
+
+copy adm-updater-rs\target\aarch64-pc-windows-msvc\release\adm-updater-rs.exe bin\Publish\ARM64\adm-updater\AutoDarkModeUpdater.exe
+copy adm-updater-rs\license.html bin\Publish\ARM64\\adm-updater\license.html
