65aa3e02ca49e4e03083aa17eed1fd2879187e81
ChenYiLins <1062790901@qq.com>
2025-05-09 11:13:49 +0800

    Regression: About DonationPage

--- AutoDarkModeApp/Views/DonationPage.xaml.cs
+++ AutoDarkModeApp/Views/DonationPage.xaml.cs
@@ -1,19 +1,31 @@
-﻿using AutoDarkModeApp.ViewModels;
-
+﻿using System.Diagnostics;
+using AutoDarkModeApp.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 
 namespace AutoDarkModeApp.Views;
 
 public sealed partial class DonationPage : Page
 {
-    public DonationViewModel ViewModel
-    {
-        get;
-    }
+    public DonationViewModel ViewModel { get; }
 
     public DonationPage()
     {
         ViewModel = App.GetService<DonationViewModel>();
         InitializeComponent();
+    }
+
+    private void DonationPayPalButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        StartProcessByProcessInfo("https://www.paypal.com/donate/?hosted_button_id=H65KZYMHKCB6E");
+    }
+
+    private void GithubSponsorsButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        StartProcessByProcessInfo("https://github.com/sponsors/Spiritreader");
+    }
+
+    private static void StartProcessByProcessInfo(string message)
+    {
+        Process.Start(new ProcessStartInfo(message) { UseShellExecute = true, Verb = "open" });
     }
 }
