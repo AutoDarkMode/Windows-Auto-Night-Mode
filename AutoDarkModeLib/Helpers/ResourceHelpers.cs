d26812213b771f5704608314a749dfb0991db4f6
ChenYiLins <1062790901@qq.com>
2025-05-18 10:02:58 +0800

    Code quality: Complete error message

--- AutoDarkModeLib/Helpers/ResourceHelpers.cs
+++ AutoDarkModeLib/Helpers/ResourceHelpers.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml.Markup;
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml.Markup;
 using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace AutoDarkModeLib.Helpers;
@@ -6,21 +8,28 @@ namespace AutoDarkModeLib.Helpers;
 [MarkupExtensionReturnType(ReturnType = typeof(string))]
 public sealed partial class ResourceString : MarkupExtension
 {
-    private static readonly ResourceManager resourceManager = new();
-    private static readonly ResourceContext resourceContext = resourceManager.CreateResourceContext();
-    private static readonly ResourceMap resourceMap = resourceManager.MainResourceMap.GetSubtree("AutoDarkModeLib/Resources");
+    private static readonly ResourceManager _resourceManager = new();
+    private static readonly ResourceContext _resourceContext = _resourceManager.CreateResourceContext();
+    private static readonly ResourceMap _resourceMap = _resourceManager.MainResourceMap.GetSubtree("AutoDarkModeLib/Resources");
 
     public string Name { get; set; } = string.Empty;
 
     protected override object ProvideValue()
     {
+        if (_resourceMap == null)
+            return $"#MISSING_MAP:{Name}";
+
         try
         {
-            return resourceMap?.GetValue(Name, resourceContext)?.ValueAsString ?? $"#{Name}";
+            return _resourceMap?.GetValue(Name, _resourceContext)?.ValueAsString ?? $"#MISSING:{Name}";
         }
-        catch
+        catch (COMException ex)
         {
-            return $"#{Name}";
+            return $"#NOT_FOUND:{Name}({ex.Message})";
+        }
+        catch (Exception ex)
+        {
+            return $"#ERROR:{Name}({ex.Message})";
         }
     }
 }
