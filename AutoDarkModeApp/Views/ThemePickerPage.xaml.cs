dce025f60ae00561110d3575f3cc13f29c0e2dc4
Chen <59414880+ChenYiLins@users.noreply.github.com>
2025-05-31 19:44:02 +0800

    Code quality: About ThemePickerPage (#971)
    * Fix: Unable to load theme correctly
    * adjust tutorial text
    * adjust InfoBar to text
    * Code quality: Visually adjust and add jump events
    * change Tutorial Description
    * tweak text
    
    Co-authored-by: Jay <65828559+Jay-o-Way@users.noreply.github.com>

--- AutoDarkModeApp/Views/ThemePickerPage.xaml.cs
+++ AutoDarkModeApp/Views/ThemePickerPage.xaml.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using AutoDarkModeApp.Helpers;
 using AutoDarkModeApp.Utils.Handlers;
 using AutoDarkModeApp.ViewModels;
 using AutoDarkModeLib;
@@ -18,17 +17,13 @@ public sealed partial class ThemePickerPage : Page
         ViewModel = App.GetService<ThemePickerViewModel>();
         InitializeComponent();
 
-        ThemeTutorialFirstStepInfoBar.Title = "Step".GetLocalized() + " 1";
-        ThemeTutorialSecondStepInfoBar.Title = "Step".GetLocalized() + " 2";
-        ThemeTutorialThirdStepInfoBar.Title = "Step".GetLocalized() + " 3";
-
         DispatcherQueue.TryEnqueue(() => LoadThemes());
     }
 
     private void LoadThemes()
     {
         List<ThemeFile> themeCollection = ThemeCollectionHandler.GetUserThemes();
-        IEnumerable<string> themeNames = themeCollection.Select(t => t.ToString());
+        IEnumerable<string> themeNames = themeCollection.Select(t => t.ToString()).ToList();
         LightThemeComboBox.ItemsSource = themeNames;
         DarkThemeComboBox.ItemsSource = themeNames;
         ThemeFile? lightSelected = themeCollection.FirstOrDefault(t => t.Path == _builder.Config.WindowsThemeMode.LightThemePath);
@@ -44,12 +39,6 @@ public sealed partial class ThemePickerPage : Page
         }
     }
 
-    private async void OpenWindowsThemePageHyperlinkButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
-        await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings:themes"));
-    }
-
-
     private void OpenThemeFolderSettingsCard_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
         var themeFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Microsoft\Windows\Themes";
@@ -63,5 +52,4 @@ public sealed partial class ThemePickerPage : Page
             Process.Start(new ProcessStartInfo(themeFolderPath) { UseShellExecute = true, Verb = "open" });
         }
     }
-
 }
