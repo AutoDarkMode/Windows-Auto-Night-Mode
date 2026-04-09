7bf02ec6a1e1902c99dfe1d26f8916a0c39dad5b
Spiritreader <sam@spiritreader.eu>
2026-04-09 21:48:53 +0200

    fix: app crash when wallpaper page is initialized and display detection has never run (#1196)

--- AutoDarkModeApp/Views/WallpaperPickerPage.xaml.cs
+++ AutoDarkModeApp/Views/WallpaperPickerPage.xaml.cs
@@ -17,8 +17,13 @@ public sealed partial class WallpaperPickerPage : Page
     {
         ViewModel = App.GetService<WallpaperPickerViewModel>();
         InitializeComponent();
-        Task.Run(DetectMonitorsAsync).Wait();
-        DispatcherQueue.TryEnqueue(LoadMonitors);
+        Loaded += WallpaperPickerPage_Loaded;
+    }
+
+    private async void WallpaperPickerPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        await DetectMonitorsAsync();
+        LoadMonitors();
     }
 
 
